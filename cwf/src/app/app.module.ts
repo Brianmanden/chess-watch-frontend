858df8ef778535a1a98428a227477084121d5fd1
Brianmanden <brianmanden@gmail.com>
2020-07-13 18:35:20 +0200

    Add dashboard with tabs containing mock data

--- cwf/src/app/app.module.ts
+++ cwf/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, DashboardComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatTabsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
